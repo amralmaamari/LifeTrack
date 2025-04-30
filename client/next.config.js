a79efa7957bda3c0688aedd930209b6a47535d76
amralmaamari <amr.almaamari@gmail.com>
2025-04-30 21:22:26 +0530

    delete vercel.json  and  next.config.js

--- client/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // switch back to SSR
-    // output: 'export',    ← make sure this line is removed or commented
-    images: { unoptimized: true },
-    eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true },
-  };
-  module.exports = nextConfig;
